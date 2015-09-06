18ab6ffa6c1d80af16c9cda0327aa96dd7abf7d1
Henrique Taunay <htaunay@gmail.com>
2015-09-06 16:02:11 -0300

    First working version of HTTP request

---
+++ client-demo/Assets/_Scripts/PointRegisterer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointRegisterer : MonoBehaviour {
+
+	void FixedUpdate ()
+	{
+		HTTP.Request someRequest = new HTTP.Request( "get", "http://rmnsvm.cloudapp.net:8081" );
+		someRequest.Send( ( request ) => {
+			// parse some JSON, for example:
+			//JSONObject thing = new JSONObject( request.response.Text );
+			Debug.Log(request.response.Text);
+			//Debug.Log( request.response.Text );
+		});
+	}
+}
