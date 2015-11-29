0b6f97664cc09edfa873bd0c07defe29de997020
Henrique Taunay <htaunay@gmail.com>
2015-11-29 16:59:45 -0200

    Double support for JSON number fields

--- client-demo/Assets/ThirdParty/JSON/JSONObject.cs
+++ client-demo/Assets/ThirdParty/JSON/JSONObject.cs
@@ -1,6 +1,6 @@
 #define PRETTY		//Comment out when you no longer need to read JSON to disable pretty Print system-wide
 //Using doubles will cause errors in VectorTemplates.cs; Unity speaks floats
-#define USEFLOAT	//Use floats for numbers instead of doubles	(enable if you're getting too many significant digits in string output)
+//#define USEFLOAT	//Use floats for numbers instead of doubles	(enable if you're getting too many significant digits in string output)
 //#define POOLING	//Currently using a build setting for this one (also it's experimental)
 
 using System.Diagnostics;
@@ -307,7 +307,7 @@ public class JSONObject {
 #if USEFLOAT
 								n = System.Convert.ToSingle(str);
 #else
-								n = System.Convert.ToDouble(str);				 
+								n = System.Convert.ToDouble(str);		 
 #endif
 								type = Type.NUMBER;
 							} catch(System.FormatException) {
--- client-demo/Assets/ThirdParty/JSON/VectorTemplates.cs
+++ client-demo/Assets/ThirdParty/JSON/VectorTemplates.cs
@@ -217,10 +217,10 @@ public static partial class JSONTemplates {
 	}
 	
 	public static Keyframe ToKeyframe(JSONObject obj){
-		Keyframe k = new Keyframe(obj.HasField("time")? obj.GetField("time").n : 0, obj.HasField("value")? obj.GetField("value").n : 0);
-		if(obj.HasField("inTangent")) k.inTangent = obj.GetField("inTangent").n;
-		if(obj.HasField("outTangent")) k.outTangent = obj.GetField("outTangent").n;
-		if(obj.HasField("tangentMode")) k.tangentMode = (int)obj.GetField("tangentMode").n;
+		Keyframe k = new Keyframe(obj.HasField("time")? (float) obj.GetField("time").n : 0, obj.HasField("value")? (float) obj.GetField("value").n : 0);
+		if(obj.HasField("inTangent")) k.inTangent = (float) obj.GetField("inTangent").n;
+		if(obj.HasField("outTangent")) k.outTangent = (float) obj.GetField("outTangent").n;
+		if(obj.HasField("tangentMode")) k.tangentMode = (int) obj.GetField("tangentMode").n;
 		
 		return k;
 	}
