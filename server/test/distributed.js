96afb909a0a5cba9e7c34640eb3c753889a41dc1
Henrique Taunay <htaunay@gmail.com>
2016-04-16 18:06:53 -0300

    Debug travis error, second try

--- server/test/distributed.js
+++ server/test/distributed.js
@@ -14,7 +14,7 @@ describe("The server\"s", function () {
 
             setTimeout(function() {
                done();    
-            }, 100);
+            }, 1000);
         }
         else {
             done();
@@ -25,18 +25,8 @@ describe("The server\"s", function () {
 
         if(test_type === undefined) {
 
-            master = exec("NODE_ENV=master mocha --timeout 10000 test/*.js > ~/.rmnslog",
+            master = exec("NODE_ENV=master mocha --timeout 10000 test/*.js",
                 function(error, stdout, stderr) {
-
-                    if(error !== null) {
-                        fs = require('fs');
-                        fs.readFile('~/.rmnslog', 'utf8', function (err,data) {
-                            if (err) {
-                                return console.log(err);
-                            }
-                            console.log(data);
-                        });
-                    }
 
                     (error === null).should.be.true();
                     done(); 
