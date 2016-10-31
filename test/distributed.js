6345e2c41b3c8b144fc89bd99a935f889857992a
Henrique Taunay <henrique@taunay.me>
2016-10-30 23:50:10 -0200

    Test Travis CI build fix, part 6

--- test/distributed.js
+++ test/distributed.js
@@ -1,6 +1,7 @@
 require("should");
 
-var exec = require("child_process").exec
+var exec = require("child_process").exec;
+var spawn = require("child_process").spawn;
 var test_type = process.env.NODE_ENV;
 var slave1, slave2, master;
 
@@ -9,8 +10,14 @@ describe("The server\"s", function () {
     before(function(done) {
         
         if(test_type === undefined) {
-            slave1 = exec("NODE_ENV=slave1 npm start", function(){});
-            slave2 = exec("NODE_ENV=slave2 npm start", function(){});
+
+            var env1 = Object.create( process.env );
+            env1.NODE_ENV = 'slave1';
+            slave1 = spawn("npm", ["start"], {"env": env1}, {"detached": true});
+
+            var env2 = Object.create( process.env );
+            env2.NODE_ENV = 'slave2';
+            slave2 = spawn("npm", ["start"], {"env": env2}, {"detached": true});
 
             // Wait for slave servers to start before running master test
             setTimeout(function() {
@@ -29,9 +36,6 @@ describe("The server\"s", function () {
             master = exec("NODE_ENV=master mocha --timeout 10000 test/*.js",
                 function(error, stdout, stderr) {
 
-                    console.log(stdout);
-                    console.log("+++++++++++++++++");
-                    console.log(stderr);
                     (error === null).should.be.true();
                     done(); 
                 }
